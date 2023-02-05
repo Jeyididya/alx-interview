a103690474f14486da3d5bb98b1747366cea41f8
Yididya <jedidiahandualem@gmail.com>
2023-02-05 09:59:52 +0300

    task 0

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -2,19 +2,19 @@
 """ Function to check if agiven boxes are open"""
 
 
-def canUnlockAll (boxes):
+def canUnlockAll(boxes):
     """Determines if boxes can be unlocked"""
-    
+
     position = 0
     unlocked = {}
 
-    for box in boxes :
-        if len (box) == 0 or position == 0 :
+    for box in boxes:
+        if len(box) == 0 or position == 0:
             unlocked[position] = "always_unlocked"
-        for key in box :
-            if key < len(boxes) and key != position :
+        for key in box:
+            if key < len(boxes) and key != position:
                 unlocked[key] = key
-        if len (unlocked) == len (boxes) :
+        if len(unlocked) == len(boxes):
             return True
         position += 1
     return False
