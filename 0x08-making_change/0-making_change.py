06b0a5dde1f14f04a3aa18ab2a94ed585eb0b52c
Yididya <jedidiahamdualem@gmail.com>
2023-03-23 17:34:42 +0300

    lint fix

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -1,8 +1,23 @@
+#!/usr/bin/python3
+"""
+Making Change
+"""
+
+
+
 def makeChange(coins, total):
-    if total <= 0:
-        return 0
-    dp = [0] + [float('inf')] * total
-    for coin in coins:
-        for i in range(coin, total + 1):
-            dp[i] = min(dp[i], dp[i - coin] + 1)
-    return dp[total] if dp[total] != float('inf') else -1
+   """
+    Return the minimum number of coins needed to meet a given total
+    Args:
+        coins (list of ints): a list of coins of different values
+        total (int): total value to be met
+    Return:
+        Number of coins or -1 if meeting the total is not possible
+  """
+  if total <= 0:
+      return 0
+  dp = [0] + [float('inf')] * total
+  for coin in coins:
+      for i in range(coin, total + 1):
+          dp[i] = min(dp[i], dp[i - coin] + 1)
+  return dp[total] if dp[total] != float('inf') else -1
