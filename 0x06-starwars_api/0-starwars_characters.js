bd10da2749711c03fdb4fb0f072f0165ae471135
Yididya <jedidiahamdualem@gmail.com>
2023-04-18 15:29:15 +0300

    [fix]-added check for errors

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,20 +1,28 @@
-#!/usr/bin/node
-
 const request = require('request');
 
 const movieId = process.argv[2];
 
-const url = `https://swapi-api.hbtn.io/api/films/${movieId}`;
+const url = `https://swapi.dev/api/films/${movieId}`;
 
 request(url, async (err, res, body) => {
-  err && console.log(err);
+  if (err) {
+    console.log(err);
+    return;
+  }
 
-  
-  const charactersArray = (JSON.parse(res.body).characters);
+  const charactersArray = JSON.parse(body).characters;
+  if (!charactersArray || charactersArray.length === 0) {
+    console.log("No characters found for this movie.");
+    return;
+  }
+
   for (const character of charactersArray) {
     await new Promise((resolve, reject) => {
       request(character, (err, res, body) => {
-        err && console.log(err);
+        if (err) {
+          console.log(err);
+          reject();
+        }
 
         console.log(JSON.parse(body).name);
         resolve();
